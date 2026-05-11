0dddacd3aac220f4e8cce6a888055b6a5654c823
maheshdarekar2005 <maheshdarekar2005@gmail.com>
2026-05-11 13:10:22 +0530

    Add files via upload

---
+++ 3sum_avg.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <omp.h>
+using namespace std;
+
+int minval(int arr[], int n){
+    int minval = arr[0];
+    
+    #pragma omp parallel for reduction(min:minval)
+    for(int i = 0; i < n; i++){
+        if(arr[i] < minval) minval = arr[i];
+    }
+        
+    return minval;
+}
+
+int maxval(int arr[],  int n){
+    int maxval = arr[0];
+    
+    #pragma omp parallel for reduction(max:maxval)
+    for(int i = 0; i < n; i++){
+        if(arr[i] > maxval) maxval = arr[i];
+    }
+    return maxval;
+}
+
+int sum(int arr[], int n){
+    int sum = 0;
+    
+    #pragma omp parallel for reduction(+:sum)
+    for(int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    
+    return sum;
+}
+
+double average(int arr[], int n)
+{
+    return (double)sum(arr, n) / n;
+    
+    }
+
+int main()
+{
+    int n;
+    
+    cout <<"No. of elements:";
+    cin >> n;
+    
+    int arr[n];
+    
+    cout <<"Elements of array:";
+    
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    cout <<"Minval is:" << minval(arr, n) << endl;
+    cout <<"Maxval is:" << maxval(arr, n) << endl;
+    cout <<"Sum is:" << sum(arr, n) << endl;
+    cout <<"Avg is:" <<average(arr, n) << endl;
+    
+    return 0;
+}
